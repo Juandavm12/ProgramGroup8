4e3136ee54b3a9584fda03d33e3d0f9f1af2650f
Juandav12 <JuanDavid@JUANDAV12>
2024-03-12 09:52:17 -0500

    Database Connection

---
+++ Veterinary.API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Veterinary.Shared.Entities;
+
+namespace Veterinary.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {       
+        }
+
+        //Registrar todas las entidades
+        public DbSet<Owner>Owners { get; set; }
+
+        //Conectar con la base de datos
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
--- Veterinary.Shared/Entities/Owner.cs
+++ Veterinary.Shared/Entities/Owner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,27 @@ namespace Veterinary.Shared.Entities
     {
         //Attributes
         public int Id { get; set; }
+
+        //Las data anotations se ponen encima del campo
+        [Display(Name = "Documento")]
+        [MaxLength(20, ErrorMessage = "No se permiten mas de 20 digitos")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string Document { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(20, ErrorMessage = "No se permiten mas de 50 caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Apellido")]
+        [MaxLength(20, ErrorMessage = "No se permiten mas de 50 caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        [MaxLength(20, ErrorMessage = "No se permiten mas de 50 caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [EmailAddress(ErrorMessage = "Digite un email valido")]
         public string Email { get; set; }
         public string FixedPhone { get; set; }
         public string CellPhone { get; set; }
